e6662694a484e6a249e1e06563cfa6f83f8c6f8e
Iara Almeida <iara.almeida.profissional@gmail.com>
2022-07-21 16:15:55 -0300

    Exercício com incluses e indexOf

---
+++ incluses-e-indexof.js
@@ -0,0 +1,14 @@
+const alunos = ['Geovana', 'Anderson', 'Gabriela', 'Pedro'];
+const notas = [8, 7, 9, 10];
+
+const alunosENotas = [alunos, notas];
+
+const localizaAluno = (nomedoAluno) => {
+    if(alunosENotas[0].includes(nomedoAluno)) {
+        const indice = alunosENotas[0].indexOf(nomedoAluno);
+            return alunosENotas[0][indice] + ', a sua nota é: ' + alunosENotas[1][indice];
+    } else {
+        return "Aluno não encontrado";
+    }
+}
+console.log(localizaAluno("Anderson"))
