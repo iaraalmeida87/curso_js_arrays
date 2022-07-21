c9b775829caf63d7dcf1ece05dc60ff15206677c
Iara Almeida <iara.almeida.profissional@gmail.com>
2022-07-20 21:24:53 -0300

    Exercício do uso do método .concat

---
+++ concat.js
@@ -0,0 +1,8 @@
+//Utilizaremos o .concat para concatenar dua listas de chamadas
+
+const turma1 = ['Amanda', 'Benicio', 'Theo'];
+const turma2 = ['Isabela', 'Arthur', 'Maitê'];
+
+const turmaUnificada = turma1.concat(turma2);
+
+console.log(`A lista das duas turmas juntas é: ${turmaUnificada}`);
