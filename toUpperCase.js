c4b8a889d7f106573b9c99b8a65c581564164169
Iara Almeida <iara.almeida.profissional@gmail.com>
2022-07-22 17:20:17 -0300

    Uso de map() e toUpperCase

---
+++ toUpperCase.js
@@ -0,0 +1,7 @@
+//Padronizando uma lista de chamada para que todos nomes fiquem com todas letras em maiúsculo
+
+const nomes = ['ana Julia', 'Caio vinicius', 'BIA silva'];
+
+const nomesPadronizados = nomes.map(nome => nome.toUpperCase())
+
+console.log(nomesPadronizados)
